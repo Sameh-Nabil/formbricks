310f83b6f5fcd9e1a80e28634e8fc155fdaff16b
Sameh Nabil <sameh305@gmail.com>
2024-02-19 22:58:14 +0200

    remove attachements from summary

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/FileUploadSummary.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/FileUploadSummary.tsx
@@ -1,15 +1,15 @@
 import Headline from "@/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/Headline";
 import { questionTypes } from "@/app/lib/questions";
 import { InboxStackIcon } from "@heroicons/react/24/solid";
-import { DownloadIcon, FileIcon } from "lucide-react";
-import Link from "next/link";
+//import { DownloadIcon, FileIcon } from "lucide-react";
+//import Link from "next/link";
 
-import { getPersonIdentifier } from "@formbricks/lib/person/util";
-import { getOriginalFileNameFromUrl } from "@formbricks/lib/storage/utils";
-import { timeSince } from "@formbricks/lib/time";
+//import { getPersonIdentifier } from "@formbricks/lib/person/util";
+//import { getOriginalFileNameFromUrl } from "@formbricks/lib/storage/utils";
+//import { timeSince } from "@formbricks/lib/time";
 import type { TSurveyQuestionSummary } from "@formbricks/types/surveys";
 import { TSurveyFileUploadQuestion } from "@formbricks/types/surveys";
-import { PersonAvatar } from "@formbricks/ui/Avatars";
+//import { PersonAvatar } from "@formbricks/ui/Avatars";
 
 interface FileUploadSummaryProps {
   questionSummary: TSurveyQuestionSummary<TSurveyFileUploadQuestion>;
@@ -18,7 +18,7 @@ interface FileUploadSummaryProps {
 
 export default function FileUploadSummary({ questionSummary, environmentId }: FileUploadSummaryProps) {
   const questionTypeInfo = questionTypes.find((type) => type.id === questionSummary.question.type);
-
+//  return;
   return (
     <div className="rounded-lg border border-slate-200 bg-slate-50 shadow-sm">
       <div className="space-y-2 px-4 pb-5 pt-6 md:px-6">
@@ -41,12 +41,12 @@ export default function FileUploadSummary({ questionSummary, environmentId }: Fi
       <div className="rounded-b-lg bg-white ">
         <div className="grid h-10 grid-cols-4 items-center border-y border-slate-200 bg-slate-100 text-sm font-bold text-slate-600">
           <div className="pl-4 md:pl-6">User</div>
-          <div className="col-span-2 pl-4 md:pl-6">Response</div>
+          <div className="col-span-2 pl-4 md:pl-6">Response {environmentId}</div>
           <div className="px-4 md:px-6">Time</div>
         </div>
-        {questionSummary.responses.map((response) => {
+        {/*questionSummary.responses.map((response) => {
           const displayIdentifier = response.person ? getPersonIdentifier(response.person) : null;
-
+            
           return (
             <div
               key={response.id}
@@ -117,7 +117,7 @@ export default function FileUploadSummary({ questionSummary, environmentId }: Fi
               <div className="px-4 text-slate-500 md:px-6">{timeSince(response.updatedAt.toISOString())}</div>
             </div>
           );
-        })}
+        })*/}
       </div>
     </div>
   );
