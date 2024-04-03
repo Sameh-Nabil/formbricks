00f97893bce607a4ed213e3c0cb3bb2f160d77d2
Sameh Nabil <sameh305@gmail.com>
2024-04-03 16:46:34 +0200

    fix name validation

--- packages/surveys/src/components/questions/OpenTextQuestion.tsx
+++ packages/surveys/src/components/questions/OpenTextQuestion.tsx
@@ -84,8 +84,8 @@ export default function OpenTextQuestion({
             autoFocus={autoFocus}
             className="border-border bg-survey-bg focus:border-border-highlight block w-full rounded-md border p-2 shadow-sm focus:outline-none focus:ring-0 sm:text-sm"
             pattern={
-              question.id == "emd6b7pqhgj0nu15ozvz3rwk" ? "(?:[\\u0600-\\u06FF]+\\s+){3,}(?:[\\u0600-\\u06FF]+\\s*)[\\u0600-\\u06FF]+" :   //Arabic Name 4 words
-              // question.id == "" ? "" :
+              question.id == "emd6b7pqhgj0nu15ozvz3rwk" ? "[\\u0600-\\u06FF\\s]+$" :   //Arabic Name 4 words
+              //question.id == "wkejmivmf45n3z9mmj72prnx" ? "\\d{10}" :   //10 digits for id number
               // question.id == "" ? "" :
               question.inputType === "phone" ? "[0-9+ ]+" : 
               ".*"
